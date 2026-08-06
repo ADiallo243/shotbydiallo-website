4fd6797380bdb7d19606615b60e0ce48d67078f1
ADiallo243 <abdoulofficiel15@gmail.com>
2026-08-05 23:21:41 -0400

    Configure Google Analytics tracking

---
+++ site-config.js
@@ -0,0 +1,4 @@
+// Public website configuration. Google Analytics Measurement IDs are not secrets.
+window.SHOTBYDIALLO_CONFIG = Object.freeze({
+  googleAnalyticsMeasurementId: "G-8TXJZXCL0W",
+});
